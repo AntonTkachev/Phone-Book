7b05d1f930f8f9dbf9272d7a8a7a08ae0f2358ed
sbt-koskovetckiy-aa <SBT-Koskovetckiy-AA@mail.ca.sbrf.ru>
2016-10-03 18:22:34 +0300

    Добавил окно предупреждения при пустом имени и номере телефона

--- src/main/scala/Interface/Utils.scala
+++ src/main/scala/Interface/Utils.scala
@@ -1,15 +1,39 @@
 package Interface
 
 import java.io.{File, FileWriter}
+import javafx.event.{ActionEvent, EventHandler}
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.stage.Stage
+
 
 object Utils {
 
   val pathToCsvFile = "test.csv"
 
+  val warningStage = new Stage()
+
+  val layoutXForScene = 220
+  val layoutYForScene = 100
+
   def writeToDB(name: String, number: String) = {
     val file = new FileWriter(new File(pathToCsvFile), true)
-    file.write(s"$name | $number;")
-    file.close()
+    if (name.nonEmpty && number.nonEmpty) {
+      file.write(s"$name | $number;")
+      file.close()
+    }
+    else {
+      val buttonOk = new Button("Заполнены не все поля")
+      warningStage.setScene(new Scene(buttonOk, layoutXForScene, layoutYForScene))
+      warningStage.setResizable(false)
+      warningStage.show()
+
+      buttonOk.setOnAction(new EventHandler[ActionEvent] {
+        override def handle(e: ActionEvent) {
+          warningStage.close()
+        }
+      })
+    }
   }
 
   def clearDB() = {
